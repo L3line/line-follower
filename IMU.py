fb438f667c243cabd0c8cc4fd2bdb7931a61af5d
Line Follower <pi@nowhere.com>
2017-05-02 13:37:35 +0000

    More IMU stuff

--- IMU.py
+++ IMU.py
@@ -4,7 +4,9 @@ class MM7150(I2C.Device):
     def __init__(self):
         busnum = I2C.get_default_bus()
         super(MM7150, self).__init__(0x40, busnum)
-
+        self.DescLen = None
+    def _getDesclen(self):
+        return 0
 
 
 imu = MM7150()
